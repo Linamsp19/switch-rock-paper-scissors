5deec846a5f0f111b1414c1bd084319ea6f2a918
Lina Sanabria <linamsp19@gmail.com>
2021-01-24 20:20:58 -0500

    The game works, but more work has to be done in order to get the game.

---
+++ switch-game.js
@@ -0,0 +1,15 @@
+var player1 = "Tijera"
+
+switch (player1) {
+    case "Tijera":
+        console.log("Draw")
+        break;
+    case "Piedra":
+        console.log("NPC wins");
+        break;
+    case "Papel":
+        console.log("Player1 wins");
+        break;
+    default :
+        console.log("Draw");
+}
