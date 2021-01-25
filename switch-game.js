b58d7d977b22c86b18f31d565f0e92a383d056d9
Lina Sanabria <linamsp19@gmail.com>
2021-01-24 20:46:07 -0500

    Inverted answers solved. It works!

--- switch-game.js
+++ switch-game.js
@@ -1,24 +1,24 @@
 var player1 = "Papel"
-var npc = "Papel"
+var npc = "Tijeras"
 
-switch (player1 === npc) {
+switch (true) {
     case player1 === "Piedra" && npc === "Papel":
-        console.log("NPC wins");
+        console.log("NPC wins.");
         break;
     case player1 === "Papel" && npc === "Piedra":
-        console.log("Player1 wins");
+        console.log("Player1 wins.");
         break;
     case player1 === "Tijeras" && npc === "Piedra":
-        console.log("NPC wins");
+        console.log("NPC wins..");
         break;
     case player1 === "Piedra" && npc === "Tijeras":
-        console.log("Player1 wins")
+        console.log("Player1 wins..")
         break;
     case player1 === "Papel" && npc === "Tijeras":
-        console.log("NPC wins")
+        console.log("NPC wins...")
         break;
     case player1 === "Tijeras" && npc === "Papel":
-        console.log("Player1 wins")
+        console.log("Player1 wins...")
         break;
     default :
         console.log("Draw");
