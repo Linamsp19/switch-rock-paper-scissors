eb0dc510001b745570cd3aa666f662d6f1ffdf12
Lina Sanabria <linamsp19@gmail.com>
2021-01-24 20:37:46 -0500

    Draw works. It seems the other options are inverted

--- switch-game.js
+++ switch-game.js
@@ -1,14 +1,24 @@
-var player1 = "Tijera"
+var player1 = "Papel"
+var npc = "Papel"
 
-switch (player1) {
-    case "Tijera":
-        console.log("Draw")
-        break;
-    case "Piedra":
+switch (player1 === npc) {
+    case player1 === "Piedra" && npc === "Papel":
         console.log("NPC wins");
         break;
-    case "Papel":
+    case player1 === "Papel" && npc === "Piedra":
         console.log("Player1 wins");
+        break;
+    case player1 === "Tijeras" && npc === "Piedra":
+        console.log("NPC wins");
+        break;
+    case player1 === "Piedra" && npc === "Tijeras":
+        console.log("Player1 wins")
+        break;
+    case player1 === "Papel" && npc === "Tijeras":
+        console.log("NPC wins")
+        break;
+    case player1 === "Tijeras" && npc === "Papel":
+        console.log("Player1 wins")
         break;
     default :
         console.log("Draw");
